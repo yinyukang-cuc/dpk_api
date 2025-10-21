4a657834bf5481310fc36f4ad9cc0841bbe25353
yinyukang <51773762+yinyukang-cuc@users.noreply.github.com>
2025-10-21 01:49:18 +0000

    deepseek api init

---
+++ main.js
@@ -0,0 +1,20 @@
+import OpenAI from 'openai'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const openai = new OpenAI({
+    baseURL:"https://api.deepseek.com",
+    apiKey:process.env.DEEPSEEK_API_KEY,
+})
+
+async function main(){
+    const completion = await openai.chat.completions.create({
+        messages:[{role:"system",content:"You are a helpful assistant."}],
+        model: "deepseek-chat",
+    })
+
+    console.log(completion.choices[0].message.content)
+}
+
+main()
---
+++ main.py
@@ -0,0 +1,4 @@
+import os
+from openapi import openapi
+
+client = OpenAI()
